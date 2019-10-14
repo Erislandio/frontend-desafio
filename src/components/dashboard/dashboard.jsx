b85d91f8151fcb7e07eee2984e616fe0eaebbbbc
Erislandio <erislandio.soares@acct.global>
2019-10-14 20:39:05 -0300

    adiçao formulario de adiçao

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./dashboard.css";
 import axios from "axios";
+import { Loading } from "../loading/loading";
 
 export default class Dashboard extends Component {
   constructor(props) {
@@ -8,7 +9,9 @@ export default class Dashboard extends Component {
 
     this.state = {
       loading: true,
-      breedsList: []
+      breedsList: [],
+      dog: "",
+      font: ""
     };
   }
 
@@ -24,17 +27,47 @@ export default class Dashboard extends Component {
         breedsList.push(key);
       });
 
-      this.setState({ breedsList, loading: false });
+      const {
+        data: { message: dog }
+      } = await axios.get(
+        `https://dog.ceo/api/breed/${breedsList[0]}/images/random`
+      );
+
+      this.setState({ breedsList, loading: false, dog });
     } catch (error) {
       console.log(error);
     }
   }
 
+  handleChangeDog = name => {};
+
   render() {
-    return (
-      <div id="dashboard">
-        <div>teste</div>
-      </div>
-    );
+    const { loading, breedsList, dog: imageDog } = this.state;
+    if (loading) {
+      return <Loading />;
+    } else {
+      return (
+        <div className="dash">
+          <img src={imageDog} id="imageDog" />
+          <div className="select-box">
+            <h3> Selecione a Raça</h3>
+            <select>
+              {breedsList.map((dog, index) => {
+                return <option key="">{dog}</option>;
+              })}
+            </select>
+            <h3> Nome do doguinho :-)</h3>
+            <input type="text" placeholder="Nome" />
+            <h3> Tipo da fonte!</h3>
+            <select>
+              {breedsList.map((dog, index) => {
+                return <option key="">{dog}</option>;
+              })}
+            </select>
+            <button>Salvar!</button>
+          </div>
+        </div>
+      );
+    }
   }
 }
