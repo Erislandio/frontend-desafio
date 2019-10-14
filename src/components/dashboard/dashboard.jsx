0d4ab768ef855a6df3c4580f7523cc4ef985cbfd
Erislandio <erislandio.soares@acct.global>
2019-10-14 19:54:15 -0300

    adiçao ciclo de vida componente

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,14 +1,40 @@
-import React, { Component } from 'react'
-import './dashboard.css'
+import React, { Component } from "react";
+import "./dashboard.css";
+import axios from "axios";
 
 export default class Dashboard extends Component {
-    render(){
-        return (
-            <div id="dashboard">
-                <div>
-                    teste
-                </div>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      breedsList: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { message }
+      } = await axios.get(`https://dog.ceo/api/breeds/list/all`);
+
+      let breedsList = [];
+
+      Object.keys(message).forEach(function(key) {
+        breedsList.push(key);
+      });
+
+      this.setState({ breedsList, loading: false });
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  render() {
+    return (
+      <div id="dashboard">
+        <div>teste</div>
+      </div>
+    );
+  }
 }
