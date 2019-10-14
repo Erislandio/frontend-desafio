4eaa07a6fe325f3a1ae8eee4b59dce848e991f51
Erislandio <erislandio.soares@acct.global>
2019-10-14 19:33:08 -0300

    adiçao do compoente dash

---
+++ src/components/dashboard/dashboar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './dashboard.css'
+
+export default class Dashboard extends Component {
+    render(){
+        return (
+            <div id="dashboard">
+                <span>Erislandio</span>
+            </div>
+        )
+    }
+}
