4b105bab652c595120b5358749680881ec91a353
Erislandio <erislandio.soares@acct.global>
2019-10-14 21:18:09 -0300

    ajustes css dash

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -11,7 +11,10 @@ export default class Dashboard extends Component {
       loading: true,
       breedsList: [],
       dog: "",
-      font: ""
+      font: "Mansalva",
+      name: "",
+      imageLoading: false,
+      color: "#fff"
     };
   }
 
@@ -39,29 +42,113 @@ export default class Dashboard extends Component {
     }
   }
 
-  handleChangeDog = name => {};
+  handleChangeBreeds = async breeds => {
+    try {
+      const {
+        data: { message }
+      } = await axios.get(`https://dog.ceo/api/breed/${breeds}/images/random`);
+
+      this.setState({ dog: message });
+    } catch (error) {}
+  };
 
   render() {
-    const { loading, breedsList, dog: imageDog } = this.state;
+    const {
+      loading,
+      breedsList,
+      dog: imageDog,
+      name,
+      font,
+      color
+    } = this.state;
+
+    const fonts = [
+      {
+        fontFamily: "Mansalva"
+      },
+      {
+        fontFamily: "Zhi Mang Xing"
+      },
+      {
+        fontFamily: "Ubuntu"
+      },
+      {
+        fontFamily: "Roboto"
+      },
+      {
+        fontFamily: "Indie Flower"
+      }
+    ];
+
+    const colors = [
+      {
+        name: "Preto",
+        color: "#2c3e50"
+      },
+      {
+        name: "Azul",
+        color: "#3498db"
+      },
+      {
+        name: "Turquesa",
+        color: "#1abc9c"
+      },
+      {
+        name: "Verde",
+        color: "#2ecc71"
+      },
+      {
+        name: "Roxo",
+        color: "#9b59b6"
+      }
+    ];
+
     if (loading) {
       return <Loading />;
     } else {
       return (
         <div className="dash">
-          <img src={imageDog} id="imageDog" />
+          <div className="imageContainer">
+            <h4 style={{ fontFamily: font, color }}>{name}</h4>
+            <img src={imageDog} id="imageDog" />
+          </div>
           <div className="select-box">
             <h3> Selecione a Raça</h3>
-            <select>
+            <select onChange={e => this.handleChangeBreeds(e.target.value)}>
               {breedsList.map((dog, index) => {
-                return <option key="">{dog}</option>;
+                return <option key={index}>{dog}</option>;
               })}
             </select>
             <h3> Nome do doguinho :-)</h3>
-            <input type="text" placeholder="Nome" />
+            <input
+              type="text"
+              placeholder="Nome"
+              onChange={e => {
+                this.setState({ name: e.target.value });
+              }}
+            />
             <h3> Tipo da fonte!</h3>
-            <select>
-              {breedsList.map((dog, index) => {
-                return <option key="">{dog}</option>;
+            <select
+              onChange={e => {
+                this.setState({ font: e.target.value });
+              }}
+            >
+              {fonts.map(({ fontFamily }, index) => {
+                return <option key={index}>{fontFamily}</option>;
+              })}
+            </select>
+            <h3> Cor da Fonte</h3>
+            <select
+              onChange={e => {
+                this.setState({ color: e.target.value });
+              }}
+            >
+              {colors.map(({ name, color: colorName }, index) => {
+                return (
+                  <option key={index} value={colorName}>
+                    {name}
+                  </option>
+                );
               })}
             </select>
             <button>Salvar!</button>
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,17 +5,22 @@ export const Footer = () => {
   return (
     <footer id="footer">
       <div>
-        <p>Desenvolvido por Erislandio Soares</p>
-      </div>
-      <div>
-        <ul>
-          <li id="linkedin">
-            <a href="https://www.linkedin.com/in/erislandio-soares-a08816141/" target="_blank" />
-          </li>
-          <li id="github">
-            <a href="https://github.com/Erislandio/" target="_blank" />
-          </li>
-        </ul>
+        <div>
+          <p>Desenvolvido por Erislandio Soares</p>
+        </div>
+        <div>
+          <ul>
+            <li id="linkedin">
+              <a
+                href="https://www.linkedin.com/in/erislandio-soares-a08816141/"
+                target="_blank"
+              />
+            </li>
+            <li id="github">
+              <a href="https://github.com/Erislandio/" target="_blank" />
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
